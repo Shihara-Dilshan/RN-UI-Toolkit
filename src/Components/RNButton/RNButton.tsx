96b9a979ddccb704fafd0c05221d8c6aa0adf8ef
Shihara Dilshan <shiharadotadilshanid@gmail.com>
2022-03-20 17:36:31 +0530

    RNButton component changes

--- src/Components/RNButton/RNButton.tsx
+++ src/Components/RNButton/RNButton.tsx
@@ -1,28 +1,63 @@
 import React from 'react';
 import {
   Text,
+  GestureResponderEvent,
   TouchableOpacity,
   View,
+  StyleProp,
+  TextStyle,
+  StyleSheet,
 } from 'react-native';
 
 export type RNButtonProps = {
   title?: string;
+  titleStyle?: StyleProp<TextStyle>;
+  backgroundStyle?: StyleProp<TextStyle>;
+  height?: number;
+  width?: number;
+  onPress?: (event: GestureResponderEvent) => void;
+  disabled?: boolean;
 };
 
 export const RNButton: React.FC<RNButtonProps> = ({
-  title = 'sddsdsdsd',
+  title = '',
+  onPress,
+  titleStyle,
+  backgroundStyle,
+  height,
+  width,
+  disabled,
 }) => {
+  const buttonContainerStyle: StyleProp<TextStyle> = StyleSheet.flatten([
+    backgroundStyle,
+    {
+      height: height,
+      width: width,
+    },
+    {
+      backgroundColor: "blue"
+    }
+  ]);
+
+  const buttonTextStyle: StyleProp<TextStyle> = StyleSheet.flatten([
+    {
+      color: 'white',
+    },
+    titleStyle
+  ]);
+
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => {}}>
-        <Text>{title}</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPressIn={onPress} disabled={disabled}>
+      <View style={buttonContainerStyle}>
+        <Text style={buttonTextStyle}>{title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 // const styles = StyleSheet.create({
-  
+
 // });
 
 RNButton.displayName = 'RNButton';
